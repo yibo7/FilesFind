eafe2edab85a39166402138079e0ddbe9bb91e28
cqs263 <cqs263@163.com>
2021-01-29 11:49:17 +0800

    项目初始化

---
+++ FilesFind/FObject.cs
@@ -0,0 +1,532 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FilesFind
+{
+    /// <summary>
+    /// 文件系统的处理对象
+    /// </summary>
+    public enum FsoMethod
+    {
+        /// <summary>
+        /// 仅用于处理文件夹
+        /// </summary>
+        Folder = 0,
+        /// <summary>
+        /// 仅用于处理文件
+        /// </summary>
+        File,
+        /// <summary>
+        /// 文件和文件夹都参与处理
+        /// </summary>
+        All
+    }
+    /// <summary>
+    /// FObject : 文件系统的处理类
+    /// </summary>
+    public abstract class FObject
+    {
+
+        # region "文件的读写操作"
+
+        /// <summary>
+        /// 以文件流的形式读取指定文件的内容
+        /// </summary>
+        /// <param name="file">指定的文件及其全路径</param>
+        /// <returns>返回 String</returns>
+        public static string ReadFile(string file)
+        {
+            string strResult = "";
+
+            FileStream fStream = new FileStream(file, System.IO.FileMode.Open, System.IO.FileAccess.Read, FileShare.ReadWrite);
+            StreamReader sReader = new StreamReader(fStream, Encoding.Default);
+
+            try
+            {
+
+                strResult = sReader.ReadToEnd();
+            }
+            catch { }
+            finally
+            {
+                fStream.Flush();
+                fStream.Close();
+                sReader.Close();
+            }
+
+            return strResult;
+        }
+
+        /// <summary>
+        /// 以文件流的形式将内容写入到指定文件中（如果该文件或文件夹不存在则创建）
+        /// </summary>
+        /// <param name="file">文件名和指定路径</param>
+        /// <param name="fileContent">文件内容</param>
+        /// <returns>返回布尔值</returns>
+        public static string WriteFile(string file, string fileContent)
+        {
+            FileInfo f = new FileInfo(file);
+            // 如果文件所在的文件夹不存在则创建文件夹
+            if (!Directory.Exists(f.DirectoryName)) Directory.CreateDirectory(f.DirectoryName);
+
+            FileStream fStream = new FileStream(file, FileMode.Create, FileAccess.Write);
+            StreamWriter sWriter = new StreamWriter(fStream, Encoding.GetEncoding("gb2312"));
+
+            try
+            {
+                sWriter.Write(fileContent);
+                return fileContent;
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+            finally
+            {
+                sWriter.Flush();
+                fStream.Flush();
+                sWriter.Close();
+                fStream.Close();
+            }
+        }
+
+        public static string WriteFileUtf8(string file, string fileContent)
+        {
+            FileInfo f = new FileInfo(file);
+            // 如果文件所在的文件夹不存在则创建文件夹
+            if (!Directory.Exists(f.DirectoryName)) Directory.CreateDirectory(f.DirectoryName);
+
+            FileStream fStream = new FileStream(file, FileMode.Create, FileAccess.Write);
+            StreamWriter sWriter = new StreamWriter(fStream, Encoding.UTF8);
+
+            try
+            {
+                sWriter.Write(fileContent);
+                return fileContent;
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+            finally
+            {
+                sWriter.Flush();
+                fStream.Flush();
+                sWriter.Close();
+                fStream.Close();
+            }
+        }
+
+        /// <summary>
+        /// 以文件流的形式将内容写入到指定文件中（如果该文件或文件夹不存在则创建）
+        /// </summary>
+        /// <param name="file">文件名和指定路径</param>
+        /// <param name="fileContent">文件内容</param>
+        /// <param name="Append">是否追加指定内容到该文件中</param>
+        /// <returns>返回布尔值</returns>
+        public static void WriteFile(string file, string fileContent, bool Append)
+        {
+            FileInfo f = new FileInfo(file);
+            // 如果文件所在的文件夹不存在则创建文件夹
+            if (!Directory.Exists(f.DirectoryName)) Directory.CreateDirectory(f.DirectoryName);
+
+            StreamWriter sWriter = new StreamWriter(file, Append, Encoding.GetEncoding("gb2312"));
+
+            try
+            {
+                sWriter.Write(fileContent);
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+            finally
+            {
+                sWriter.Flush();
+                sWriter.Close();
+            }
+        }
+
+        /// <summary>
+        /// 以文件流的形式将内容写入到指定文件中（如果该文件或文件夹不存在则创建）-自定义编码
+        /// </summary>
+        /// <param name="file">文件名和指定路径</param>
+        /// <param name="fileContent">文件内容</param>
+        /// <param name="Append">是否追加指定内容到该文件中</param>
+        /// <returns>返回布尔值</returns>
+        public static void WriteFile(string file, string fileContent, bool Append, Encoding ec)
+        {
+            FileInfo f = new FileInfo(file);
+            // 如果文件所在的文件夹不存在则创建文件夹
+            if (!Directory.Exists(f.DirectoryName)) Directory.CreateDirectory(f.DirectoryName);
+
+            StreamWriter sWriter = new StreamWriter(file, Append, ec);
+
+            try
+            {
+                sWriter.Write(fileContent);
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+            finally
+            {
+                sWriter.Flush();
+                sWriter.Close();
+            }
+        }
+        # endregion
+
+
+        # region "文件夹信息的读取"
+
+
+
+
+        /// <summary>
+        /// 获取指定文件夹的信息，如：文件夹大小，文件夹数，文件数
+        /// </summary>
+        /// <param name="dir">指定文件夹路径</param>
+        /// <returns>返回 String</returns>
+        public static long[] getDirInfos(string dir)
+        {
+            long[] intResult = new long[3];
+            DirectoryInfo d = new DirectoryInfo(dir);
+            intResult = DirInfo(d);
+
+            return intResult;
+        }
+
+
+        private static long[] DirInfo(DirectoryInfo d)
+        {
+            long[] intResult = new long[3];
+
+            long Size = 0;
+            long Dirs = 0;
+            long Files = 0;
+            // 计算文件大小
+            FileInfo[] files = d.GetFiles();
+            Files += files.Length;
+            foreach (FileInfo file in files)
+            {
+                Size += file.Length;
+            }
+            // 计算文件夹
+            DirectoryInfo[] dirs = d.GetDirectories();
+            Dirs += dirs.Length;
+            foreach (DirectoryInfo dir in dirs)
+            {
+                Size += DirInfo(dir)[0];
+                Dirs += DirInfo(dir)[1];
+                Files += DirInfo(dir)[2];
+            }
+
+            intResult[0] = Size;
+            intResult[1] = Dirs;
+            intResult[2] = Files;
+            return intResult;
+        }
+
+
+
+        # endregion
+
+
+        # region "文件系统的相应操作"
+
+        /// <summary>
+        /// 判断文件或文件夹是否存在
+        /// </summary>
+        /// <param name="file">指定文件及其路径</param>
+        /// <param name="method">判断方式</param>
+        /// <returns>返回布尔值</returns>
+        public static bool IsExist(string file, FsoMethod method)
+        {
+            try
+            {
+                if (method == FsoMethod.File)
+                {
+                    return File.Exists(file);
+                }
+                else if (method == FsoMethod.Folder)
+                {
+                    return Directory.Exists(file);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+        }
+
+        # region "新建"
+
+        /// <summary>
+        /// 新建文件或文件夹
+        /// </summary>
+        /// <param name="file">文件或文件夹及其路径</param>
+        /// <param name="method">新建方式</param>
+        public static void Create(string file, FsoMethod method)
+        {
+            try
+            {
+                if (method == FsoMethod.File)
+                {
+                    WriteFile(file, "");
+                }
+                else if (method == FsoMethod.Folder)
+                {
+                    Directory.CreateDirectory(file);
+                }
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+        }
+
+        # endregion
+
+
+        # region "复制"
+
+        # region "复制文件"
+        /// <summary>
+        /// 复制文件，如果目标文件已经存在则覆盖掉
+        /// </summary>
+        /// <param name="oldFile">源文件</param>
+        /// <param name="newFile">目标文件</param>
+        public static void CopyFile(string oldFile, string newFile)
+        {
+            try
+            {
+                File.Copy(oldFile, newFile, true);
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+        }
+
+
+        /// <summary>
+        /// 以流的形式复制拷贝文件
+        /// </summary>
+        /// <param name="oldPath">源文件</param>
+        /// <param name="newPath">目标文件</param>
+        /// <returns></returns>
+        public static bool CopyFileStream(string oldPath, string newPath)
+        {
+            try
+            {
+                //建立两个FileStream对象
+                FileStream fsOld = new FileStream(oldPath, FileMode.Open, FileAccess.Read);
+                FileStream fsNew = new FileStream(newPath, FileMode.Create, FileAccess.Write);
+
+                //分别建立一个读写类
+                BinaryReader br = new BinaryReader(fsOld);
+                BinaryWriter bw = new BinaryWriter(fsNew);
+
+                //将读取文件流的指针指向流的头部
+                br.BaseStream.Seek(0, SeekOrigin.Begin);
+                //将写入文件流的指针指向流的尾部
+                br.BaseStream.Seek(0, SeekOrigin.End);
+
+                while (br.BaseStream.Position < br.BaseStream.Length)
+                {
+                    //从br流中读取一个Byte并马上写入bw流
+                    bw.Write(br.ReadByte());
+                }
+                //释放所有被占用的资源
+                br.Close();
+                bw.Close();
+                fsOld.Flush();
+                fsOld.Close();
+                fsNew.Flush();
+                fsNew.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        # endregion
+
+        # region "复制文件夹"
+
+        /// <summary>
+        /// 复制文件夹中的所有内容及其子目录所有文件
+        /// </summary>
+        /// <param name="oldDir">源文件夹及其路径</param>
+        /// <param name="newDir">目标文件夹及其路径</param>
+        public static void CopyDirectory(string oldDir, string newDir)
+        {
+            try
+            {
+                DirectoryInfo dInfo = new DirectoryInfo(oldDir);
+                CopyDirInfo(dInfo, oldDir, newDir);
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+        }
+
+        private static void CopyDirInfo(DirectoryInfo od, string oldDir, string newDir)
+        {
+            // 寻找文件夹
+            if (!IsExist(newDir, FsoMethod.Folder)) Create(newDir, FsoMethod.Folder);
+            DirectoryInfo[] dirs = od.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                CopyDirInfo(dir, dir.FullName, newDir + dir.FullName.Replace(oldDir, ""));
+            }
+            // 寻找文件
+            FileInfo[] files = od.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                CopyFile(file.FullName, newDir + file.FullName.Replace(oldDir, ""));
+            }
+        }
+
+        # endregion
+
+        # endregion
+        /// <summary>
+        /// 检查文件对应的目录是否存在，不存在就创建目录
+        /// </summary>
+        /// <param name="sPath">路径</param>
+        public static void ExistsDirectory(string sPath)
+        {
+            FileInfo f = new FileInfo(sPath);
+            // 如果文件所在的文件夹不存在则创建文件夹
+            if (!Directory.Exists(f.DirectoryName)) Directory.CreateDirectory(f.DirectoryName);
+        }
+
+        # region "移动"
+
+        /// <summary>
+        /// 移动文件或文件夹
+        /// </summary>
+        /// <param name="oldFile">原始文件或文件夹</param>
+        /// <param name="newFile">目标文件或文件夹</param>
+        /// <param name="method">移动方式：1、为移动文件，2、为移动文件夹</param>
+        public static void Move(string oldFile, string newFile, FsoMethod method)
+        {
+            try
+            {
+                FileInfo f = new FileInfo(newFile);
+                // 如果文件所在的文件夹不存在则创建文件夹
+                if (!Directory.Exists(f.DirectoryName)) Directory.CreateDirectory(f.DirectoryName);
+
+
+                if (method == FsoMethod.File)
+                {
+                    File.Move(oldFile, newFile);
+                }
+                if (method == FsoMethod.Folder)
+                {
+                    Directory.Move(oldFile, newFile);
+                }
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+        }
+
+        # endregion
+
+
+        # region "删除"
+
+        /// <summary>
+        /// 删除文件或文件夹
+        /// </summary>
+        /// <param name="file">文件或文件夹及其路径</param>
+        /// <param name="method">删除方式：1、为删除文件，2、为删除文件夹</param>
+        public static void Delete(string file, FsoMethod method)
+        {
+            try
+            {
+                if (method == FsoMethod.File)
+                {
+                    File.Delete(file);
+                }
+                if (method == FsoMethod.Folder)
+                {
+                    Directory.Delete(file, true);//删除该目录下的所有文件以及子目录
+                }
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.ToString());
+            }
+        }
+
+
+        /// <summary>
+        /// 删除某个目录下所有文件，保留目录- 删除目录再创建相同目录
+        /// </summary>
+        /// <param name="strDir">目录地址</param>
+        public static void DeleteFiles(string strDir)
+        {
+            if (Directory.Exists(strDir))
+            {
+                Directory.Delete(strDir, true);
+                Directory.CreateDirectory(strDir);
+                //Console.WriteLine("文件删除成功！");
+            }
+            else
+            {
+                //Console.WriteLine("此目录不存在！");
+            }
+        }
+
+        # endregion
+
+        # endregion
+
+
+
+
+
+        public static FileInfo[] GetFileListByType(string dirpath, string Type)
+        {
+            DirectoryInfo info;
+            info = new DirectoryInfo(dirpath);
+
+            return info.GetFiles("*." + Type);
+        }
+
+        public static List<FileInfo> GetFileListByTypes(string dirpath, string[] Types)
+        {
+            List<FileInfo> list = new List<FileInfo>();
+            foreach (string str in Types)
+            {
+                FileInfo[] fileListByType = GetFileListByType(dirpath, str);
+                foreach (FileInfo info in fileListByType)
+                {
+                    list.Add(info);
+                }
+            }
+            return list;
+        }
+
+
+
+
+    }
+}
---
+++ FilesFind/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FilesFind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel6 = new System.Windows.Forms.ToolStripLabel();
+            this.txtFilesPath = new System.Windows.Forms.ToolStripTextBox();
+            this.btnSelPathForWatch = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel5 = new System.Windows.Forms.ToolStripLabel();
+            this.txtFileType = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator9 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnToSetting = new System.Windows.Forms.ToolStripButton();
+            this.lstFiles = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbFindingInfo = new System.Windows.Forms.Label();
+            this.stTool = new System.Windows.Forms.StatusStrip();
+            this.stlbSearchTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.stTool.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel6,
+            this.txtFilesPath,
+            this.btnSelPathForWatch,
+            this.toolStripLabel5,
+            this.txtFileType,
+            this.toolStripSeparator9,
+            this.btnToSetting});
+            this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(882, 39);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel6
+            // 
+            this.toolStripLabel6.Name = "toolStripLabel6";
+            this.toolStripLabel6.Size = new System.Drawing.Size(32, 36);
+            this.toolStripLabel6.Text = "目录";
+            // 
+            // txtFilesPath
+            // 
+            this.txtFilesPath.Name = "txtFilesPath";
+            this.txtFilesPath.Size = new System.Drawing.Size(200, 39);
+            // 
+            // btnSelPathForWatch
+            // 
+            this.btnSelPathForWatch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSelPathForWatch.Image = ((System.Drawing.Image)(resources.GetObject("btnSelPathForWatch.Image")));
+            this.btnSelPathForWatch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSelPathForWatch.Name = "btnSelPathForWatch";
+            this.btnSelPathForWatch.Size = new System.Drawing.Size(36, 36);
+            this.btnSelPathForWatch.Text = "toolStripButton8";
+            this.btnSelPathForWatch.Click += new System.EventHandler(this.btnSelPathForWatch_Click);
+            // 
+            // toolStripLabel5
+            // 
+            this.toolStripLabel5.Name = "toolStripLabel5";
+            this.toolStripLabel5.Size = new System.Drawing.Size(32, 36);
+            this.toolStripLabel5.Text = "类型";
+            // 
+            // txtFileType
+            // 
+            this.txtFileType.Name = "txtFileType";
+            this.txtFileType.Size = new System.Drawing.Size(200, 39);
+            this.txtFileType.Text = ".txt";
+            // 
+            // toolStripSeparator9
+            // 
+            this.toolStripSeparator9.Name = "toolStripSeparator9";
+            this.toolStripSeparator9.Size = new System.Drawing.Size(6, 39);
+            // 
+            // btnToSetting
+            // 
+            this.btnToSetting.Image = ((System.Drawing.Image)(resources.GetObject("btnToSetting.Image")));
+            this.btnToSetting.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnToSetting.Name = "btnToSetting";
+            this.btnToSetting.Size = new System.Drawing.Size(92, 36);
+            this.btnToSetting.Text = "开始查找";
+            this.btnToSetting.Click += new System.EventHandler(this.btnToSetting_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFiles.Location = new System.Drawing.Point(0, 39);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(882, 592);
+            this.lstFiles.TabIndex = 10;
+            this.lstFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbFindingInfo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 572);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(882, 37);
+            this.panel1.TabIndex = 14;
+            // 
+            // lbFindingInfo
+            // 
+            this.lbFindingInfo.AutoSize = true;
+            this.lbFindingInfo.ForeColor = System.Drawing.Color.Red;
+            this.lbFindingInfo.Location = new System.Drawing.Point(4, 11);
+            this.lbFindingInfo.Name = "lbFindingInfo";
+            this.lbFindingInfo.Size = new System.Drawing.Size(0, 12);
+            this.lbFindingInfo.TabIndex = 2;
+            // 
+            // stTool
+            // 
+            this.stTool.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stlbSearchTime});
+            this.stTool.Location = new System.Drawing.Point(0, 609);
+            this.stTool.Name = "stTool";
+            this.stTool.Size = new System.Drawing.Size(882, 22);
+            this.stTool.TabIndex = 13;
+            this.stTool.Text = "statusStrip1";
+            // 
+            // stlbSearchTime
+            // 
+            this.stlbSearchTime.Name = "stlbSearchTime";
+            this.stlbSearchTime.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 631);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.stTool);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.stTool.ResumeLayout(false);
+            this.stTool.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel6;
+        private System.Windows.Forms.ToolStripTextBox txtFilesPath;
+        private System.Windows.Forms.ToolStripButton btnSelPathForWatch;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel5;
+        private System.Windows.Forms.ToolStripTextBox txtFileType;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator9;
+        private System.Windows.Forms.ToolStripButton btnToSetting;
+        private System.Windows.Forms.ListView lstFiles;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbFindingInfo;
+        private System.Windows.Forms.StatusStrip stTool;
+        private System.Windows.Forms.ToolStripStatusLabel stlbSearchTime;
+    }
+}
+
